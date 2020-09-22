df0968f23cbfaba4421a2d74dda0e0c4a0717c9d
gec-erken-ugrayan <umithief2@gmail.com>
2020-09-22 23:35:08 +0300

    listing by category

---
+++ NortwindContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Listing.Entities;
+
+namespace Listing
+{
+    public class NortwindContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
